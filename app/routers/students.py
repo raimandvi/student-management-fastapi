25aaeb0247accb5f01b9ae08a56074b663299c83
raimandvi <raimandvi123@gmail.com>
2026-01-20 21:38:48 +0530

    add student API routes CRUD endpoints.

--- app/routers/students.py
+++ app/routers/students.py
@@ -1,10 +1,56 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
+from .. import crud, schemas
+from ..database import get_db
 router = APIRouter(
     prefix="/students",
     tags=["Students"]
 )
 
-@router.get("/")
-def get_students():
-    return {"message": "Students API working"}
+
+@router.post("/", response_model=schemas.StudentResponse)
+def create_student(
+    student: schemas.StudentCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.create_student(db, student)
+
+
+@router.get("/", response_model=list[schemas.StudentResponse])
+def read_students(db: Session = Depends(get_db)):
+    return crud.get_students(db)
+
+
+@router.get("/{student_id}", response_model=schemas.StudentResponse)
+def read_student(student_id: int, db: Session = Depends(get_db)):
+    student = crud.get_student_by_id(db, student_id)
+
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    return student
+
+
+@router.put("/{student_id}", response_model=schemas.StudentResponse)
+def update_student(
+    student_id: int,
+    student: schemas.StudentCreate,
+    db: Session = Depends(get_db)
+):
+    updated_student = crud.update_student(db, student_id, student)
+
+    if not updated_student:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    return updated_student
+
+
+@router.delete("/{student_id}")
+def delete_student(student_id: int, db: Session = Depends(get_db)):
+    deleted_student = crud.delete_student(db, student_id)
+
+    if not deleted_student:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    return {"message": "Student deleted successfully"}
