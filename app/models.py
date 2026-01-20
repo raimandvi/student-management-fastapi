0ee6b5a634c8d7e771a673df852b71b594ecb487
raimandvi <raimandvi123@gmail.com>
2026-01-20 21:36:58 +0530

    define Student model using SQLAlchemy ORM

--- app/models.py
+++ app/models.py
@@ -4,7 +4,8 @@ from app.database import Base
 class Student(Base):
     __tablename__ = "students"
 
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    age = Column(Integer)
-    email = Column(String, unique=True, index=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    email = Column(String, unique=True, nullable=False)
